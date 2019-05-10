d327bbcd54ef283134bc244e0904706b88f3f202
Fulton Zink <joeyzink@gmail.com>
2019-05-10 11:30:26 -0700

    Add support for async/await

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,43 +1,48 @@
 const webpack = require('webpack');
+const path = require('path');
+
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const APP_DIR = path.resolve(__dirname, '../src');
+
 module.exports = env => {
     const { PLATFORM, VERSION } = env;
     return merge([
         {
-          module: {
-            rules: [
-              {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader'
+            entry: ['@babel/polyfill', APP_DIR],
+            module: {
+                rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                    loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                    PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                    ]
                 }
-              },
-              {
-                test: /\.scss$/,
-                use: [
-                  PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-                  'css-loader',
-                  'sass-loader'
                 ]
-              }
-            ]
-          },
-          plugins: [
-            new HtmlWebpackPlugin({
-              template: './src/index.html',
-              filename: './index.html'
-            }),
-            new webpack.DefinePlugin({ 
-              'process.env.VERSION': JSON.stringify(env.VERSION),
-              'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
-            }),
-            new CopyWebpackPlugin([ { from: 'src/static' } ]),
-          ],
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: './src/index.html',
+                    filename: './index.html'
+                }),
+                new webpack.DefinePlugin({ 
+                    'process.env.VERSION': JSON.stringify(env.VERSION),
+                    'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+                }),
+                new CopyWebpackPlugin([ { from: 'src/static' } ]),
+            ],
         }
     ])
 };
