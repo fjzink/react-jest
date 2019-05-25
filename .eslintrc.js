a6d1dda1e714c33bed18d80eec1f05f254d18db2
Fulton Zink <joeyzink@gmail.com>
2019-05-25 14:30:07 -0700

    Setup jest.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,8 @@ module.exports = {
 		}
 	},
 	rules: {
+		'react/prop-types': 'off',
 		'linebreak-style': 'off',
-
 		'arrow-parens': 'off',
 		'object-curly-newline': 'off',
 		'no-mixed-operators': 'off',
@@ -31,25 +31,19 @@ module.exports = {
 		'function-paren-newline': 'off',
 		'no-plusplus': 'off',
 		'space-before-function-paren': 0,
-
 		'max-len': ['error', 100, 2, { ignoreUrls: true, }],
 		'no-console': 'error',
 		'no-alert': 'error',
-
 		'no-param-reassign': 'off',
 		"radix": "off",
-
 		'react/require-default-props': 'off',
 		'react/forbid-prop-types': 'off',
 		'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
-
 		'prefer-destructuring': 'off',
-
 		'react/no-find-dom-node': 'off',
 		'react/no-did-mount-set-state': 'off',
 		'react/no-unused-prop-types': 'off',
 		'react/jsx-one-expression-per-line': 'off',
-
 		"jsx-a11y/anchor-is-valid": ["error", { "components": ["Link"], "specialLink": ["to"] }],
 		"jsx-a11y/label-has-for": [2, {
 			"required": {
